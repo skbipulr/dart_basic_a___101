ce169d0b4b83583d4605163d3f5b3a3e26100735
skbipulr <skbipulr@gmail.com>
2022-02-13 11:06:03 +0600

    02. problem solved

--- bin/main.dart
+++ bin/main.dart
@@ -2,7 +2,14 @@
 
 void main() {
 
+  //01. problem solved
   print("Hello\nBipul Sarkar");
+
+  ////02. problem solved
+  int fristNumber = 101;
+  int secondNumber = 201;
+  int sum = fristNumber + secondNumber;
+  print("Sum: $sum");
 
 
 }
