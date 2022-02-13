a6691e17308c28f7db209332ab118e991074aacf
skbipulr <skbipulr@gmail.com>
2022-02-13 11:49:26 +0600

    03. and 04. problem solved

--- bin/main.dart
+++ bin/main.dart
@@ -1,15 +1,47 @@
 
 
+import 'dart:io';
+
 void main() {
 
   //01. problem solved
   print("Hello\nBipul Sarkar");
 
-  ////02. problem solved
+  //02. problem solved
   int fristNumber = 101;
   int secondNumber = 201;
   int sum = fristNumber + secondNumber;
   print("Sum: $sum");
 
+  //03. problem solved
+  declareMethod();
+
+  //04. problem solved
+  print("Enter your frist number: ");
+  int? fristNum = int.parse(stdin.readLineSync()!);
+
+  print("Enter your second number: ");
+  int? secondNum = int.parse(stdin.readLineSync()!);
+  int total = fristNum + secondNum;
+  print('User input total: $fristNum + $secondNum = $total');
+
 
 }
+
+
+
+
+void declareMethod(){
+  int num = 12;
+  String stNum = '12';
+  String myName = "Bipul Sarkar";
+  double dNum = 23.4;
+  var nameList = ['Bipul Sarkar', 'Khogendro Sarkar','Nioty Sarkar'];
+  var num_list = [1,2,3];
+  var student = {'id':'110', 'name':'Bipul sarkar'};
+
+
+
+
+}
+
